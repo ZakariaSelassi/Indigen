55ebabcb0e94986c499ac533d7f19384757b71fa
nataliacherman08 <cherman.natalia08@gmail.com>
2021-06-10 14:05:55 +0200

    prev disabled

--- js/galerie.js
+++ js/galerie.js
@@ -7,6 +7,37 @@ let index = 1;
 
 const pagination = Math.ceil(photosGalerie.length / maxPhotos); /*Calcul du ratio de photos qui sert dans le calcul de showPhotos */
 
+/* Relie les boutons prev et next du HTML et lance les fonctions check() et showPhotos()
+Pour le prev l'index diminue 
+Pour le next l'index augmente */
+prev.addEventListener("click", function(){
+    index--;
+    check();
+    showPhotos();
+})
+next.addEventListener("click", function(){
+    index++;
+    check();
+    showPhotos();
+})
+
+/* Fonction qui permet de désactiver le "prev" / "next" par rapport à l'index 
+le disabled est lui aussi défini dans street.scss
+classlist vient ajouter cette classe de manière fictive*/
+function check() {
+    if(index==pagination) {
+        next.classList.add("disabled");
+    } else {
+        next.classList.remove("disabled");
+    }
+
+    if(index == 1) {
+        prev.classList.add("disabled");
+    } else {
+        prev.classList.remove("disabled");
+    }
+}
+
 /*Cette fonction permet de n'afficher qu'une partie des photos de la galerie (selon l'index)
 classlist vient ajouter de manière fictive la classe show ou hide selon index et le maxPhotos(déclaré plus haut) de la Galerie 
 Show et hide sont définis dans street.scss*/
@@ -23,38 +54,8 @@ function showPhotos() {
     }
 }
 
-/* Fonction qui permet de désactiver le "prev" / "next" par rapport à l'index 
-le disabled est lui aussi défini dans street.scss
-classlist vient ajouter cette classe de manière fictive*/
-function check() {
-    if(index == 1 ) {
-        prev.classList.add("disabled");
-        console.log(index);
-    } else {
-        prev.classList.remove("disabled");
-        console.log(index);
-    }
 
-    if(index==pagination) {
-        next.classList.add("disabled");
-    } else {
-        next.classList.remove("disabled");
-    }
-
-}
-
-/* Relie les boutons prev et next du HTML et lance les fonctions check() et showPhotos()
-Pour le prev l'index diminue 
-Pour le next l'index augmente */
-prev.addEventListener("click", function(){
-    index--;
-    check();
+window.onload = function(){
     showPhotos();
-})
-next.addEventListener("click", function(){
-    index++;
     check();
-    showPhotos();
-})
-
-window.onload = showPhotos();
+};
